940d4e05b56608259395b66db5e8c07d8fb551a5
Nick Muerdter <12112+GUI@users.noreply.github.com>
2019-11-11 09:53:34 -0700

    Add missing package dependency and add tests.

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -Eeuxo pipefail
+
+export PGUSER="$POSTGRES_USER"
+export PGDATABASE="$POSTGRES_DB"
+
+psql -v ON_ERROR_STOP=1 -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+
+extension_count=$(psql -v ON_ERROR_STOP=1 -A -t -c "SELECT COUNT(*) FROM pg_extension WHERE extname = 'postgis';")
+if [ "$extension_count" = "1" ]; then
+  echo "PostGIS Tests: OK"
+else
+  echo "PostGIS Tests: Error"
+fi
+
+pg_ctl -D "$PGDATA" stop
